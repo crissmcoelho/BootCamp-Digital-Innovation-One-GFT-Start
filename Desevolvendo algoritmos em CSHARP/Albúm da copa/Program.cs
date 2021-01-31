dc219e87d5b4bc022bf1909bb533cb8c04b54ffe
crissmcoelho <75638966+crissmcoelho@users.noreply.github.com>
2021-01-31 13:10:08 -0300

    Atividades Bootcamp GFT 2021
    Algoritmos desenvolvido em C# GFT 2021.

---
+++ Desevolvendo algoritmos em CSHARP/Albúm da copa/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albúm_da_copa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+
+            int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+
+            int totalDeFigurinhas = 0;
+
+
+
+            int[] albumDeFigurinha = new int[numeroDeFigurinhas];
+
+
+
+            // IMPLEMENTE AQUI A SOLUÇÃO
+
+            for (int i = 0; i < numeroDeFigurinhasCompradas; i++)
+
+            {
+
+                try
+
+                {
+
+                    if ((numeroDeFigurinhas > 100) || (numeroDeFigurinhas < 1))
+
+                    {
+
+                        break;
+
+                    }
+
+                    if ((numeroDeFigurinhasCompradas > 300) || (numeroDeFigurinhasCompradas < 1))
+
+                    {
+
+                        break;
+
+                    }
+
+                    int figurinha = int.Parse(Console.ReadLine());
+
+                    if (!albumDeFigurinha.Contains(figurinha))
+
+                    {
+
+                        albumDeFigurinha[i] = figurinha;
+
+                        numeroDeFigurinhas -= 1;
+
+                    }
+
+                }
+
+                catch
+
+                {
+
+                    break;
+
+                }
+
+            }
+
+            totalDeFigurinhas = numeroDeFigurinhas;
+
+
+
+            Console.WriteLine(totalDeFigurinhas);
+
+        }
+
+    }
+
---
+++ Desevolvendo algoritmos em CSHARP/Cálculo de viagem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_de_viagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            int t, v;
+            double l;
+
+            input = Console.ReadLine().Split(" ");
+            t = int.Parse(input[0]);
+            v = int.Parse(input[1]);
+
+            l = t * v / 12.00;
+
+            Console.WriteLine(l.ToString("N3", CultureInfo.InvariantCulture));
+        }
+    }
+}
+        
